95012ada9cfdb455c11e69928ece5d2f0e7cf47b
Andrei Grazhdan <myway@yandex-team.ru>
2022-08-06 14:11:54 +0700

    Добавлен каталог и класс src/SalesManager.java Добавлен класс Main.java Реализована работа классов, в консоль добавлена демонстрация. В файле README.md создано описание работы классов.

---
+++ Main.java
@@ -0,0 +1,15 @@
+import src.SalesManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] salesManager1 = {67, 3, 7, 23};
+        int[] salesManager2 = {1, 3, 45, 238};
+
+        int[] sales = salesManager1;
+
+        SalesManager manager = new SalesManager(sales);
+        System.out.println(manager.max());
+    }
+}
