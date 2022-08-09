c7e38828b55e5a0bcd59de439adbaf49720ea55b
Andrei Grazhdan <myway@yandex-team.ru>
2022-08-09 18:37:29 +0700

    реализован метод calcStat

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] salesManager1 = {67, 3, 7, 23};
+        int[] salesManager1 = {67, 3, 7, 23,455,200,2};
         int[] salesManager2 = {1, 3, 45, 238};
 
         int[] sales = salesManager1;
 
         SalesManager manager = new SalesManager(sales);
-        System.out.println(manager.max());
+        System.out.println("Максимальная по сумме продажа " + manager.max());
+        System.out.println("Средняя сумма продаж " + manager.calcStat());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,5 +1,8 @@
 package src;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class SalesManager {
     protected int[] sales;
 
@@ -15,5 +18,27 @@ public class SalesManager {
             }
         }
         return max;
+    }
+    public int calcStat(){
+        int calcStat = 0;
+        int min = sales[0];
+        int max = sales[0];
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            if (sale < min) {
+                min = sale;
+            }
+            sum = sum + sale;
+        }
+        int crop = sum - min - max;
+        if (sales.length > 0) {
+            calcStat = crop / (sales.length - 2);
+        } else {
+            System.out.println("Укажите продажи менеджера!");
+        }
+        return calcStat;
     }
 }
